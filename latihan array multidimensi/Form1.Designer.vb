effd3b97375d7d3474344c64342195b9b2f63dea
jordansulaeman1928 <jordansulaeman1928@gmail.com>
2024-11-27 20:14:46 +0700

    Commit Awal

---
+++ latihan array multidimensi/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        NPM = New TextBox()
+        label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        txtnama = New TextBox()
+        btnproses = New Button()
+        List1 = New ListView()
+        cbokelamin = New ComboBox()
+        cboprodi = New ComboBox()
+        btnedit = New Button()
+        btnhapus = New Button()
+        btnkeluar = New Button()
+        SuspendLayout()
+        ' 
+        ' NPM
+        ' 
+        NPM.CharacterCasing = CharacterCasing.Upper
+        NPM.Location = New Point(151, 35)
+        NPM.Name = "NPM"
+        NPM.Size = New Size(121, 23)
+        NPM.TabIndex = 0
+        ' 
+        ' label1
+        ' 
+        label1.AutoSize = True
+        label1.Location = New Point(12, 35)
+        label1.Name = "label1"
+        label1.Size = New Size(34, 15)
+        label1.TabIndex = 1
+        label1.Text = "NPM"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(12, 67)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(43, 15)
+        Label2.TabIndex = 2
+        Label2.Text = "NAMA"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 107)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(88, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "JENIS KELAMIN"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(12, 146)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(98, 15)
+        Label4.TabIndex = 4
+        Label4.Text = "PROGRAM STUDI"
+        ' 
+        ' txtnama
+        ' 
+        txtnama.CharacterCasing = CharacterCasing.Upper
+        txtnama.Location = New Point(151, 67)
+        txtnama.Name = "txtnama"
+        txtnama.Size = New Size(121, 23)
+        txtnama.TabIndex = 2
+        ' 
+        ' btnproses
+        ' 
+        btnproses.Location = New Point(613, 184)
+        btnproses.Name = "btnproses"
+        btnproses.Size = New Size(111, 28)
+        btnproses.TabIndex = 5
+        btnproses.Text = "Proses"
+        btnproses.UseVisualStyleBackColor = True
+        ' 
+        ' List1
+        ' 
+        List1.Location = New Point(27, 218)
+        List1.Name = "List1"
+        List1.Size = New Size(717, 161)
+        List1.TabIndex = 6
+        List1.UseCompatibleStateImageBehavior = False
+        ' 
+        ' cbokelamin
+        ' 
+        cbokelamin.DropDownStyle = ComboBoxStyle.DropDownList
+        cbokelamin.FormattingEnabled = True
+        cbokelamin.Location = New Point(151, 107)
+        cbokelamin.Name = "cbokelamin"
+        cbokelamin.Size = New Size(133, 23)
+        cbokelamin.TabIndex = 3
+        ' 
+        ' cboprodi
+        ' 
+        cboprodi.DropDownStyle = ComboBoxStyle.DropDownList
+        cboprodi.FormattingEnabled = True
+        cboprodi.Location = New Point(151, 146)
+        cboprodi.Name = "cboprodi"
+        cboprodi.Size = New Size(133, 23)
+        cboprodi.TabIndex = 4
+        ' 
+        ' btnedit
+        ' 
+        btnedit.Location = New Point(479, 184)
+        btnedit.Name = "btnedit"
+        btnedit.Size = New Size(111, 28)
+        btnedit.TabIndex = 7
+        btnedit.Text = "Edit"
+        btnedit.UseVisualStyleBackColor = True
+        ' 
+        ' btnhapus
+        ' 
+        btnhapus.Location = New Point(346, 184)
+        btnhapus.Name = "btnhapus"
+        btnhapus.Size = New Size(113, 28)
+        btnhapus.TabIndex = 8
+        btnhapus.Text = "Hapus"
+        btnhapus.UseVisualStyleBackColor = True
+        ' 
+        ' btnkeluar
+        ' 
+        btnkeluar.Location = New Point(613, 398)
+        btnkeluar.Name = "btnkeluar"
+        btnkeluar.Size = New Size(111, 23)
+        btnkeluar.TabIndex = 9
+        btnkeluar.Text = "Keluar"
+        btnkeluar.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnkeluar)
+        Controls.Add(btnhapus)
+        Controls.Add(btnedit)
+        Controls.Add(cboprodi)
+        Controls.Add(cbokelamin)
+        Controls.Add(List1)
+        Controls.Add(btnproses)
+        Controls.Add(txtnama)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(label1)
+        Controls.Add(NPM)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents NPM As TextBox
+    Friend WithEvents label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtnama As TextBox
+    Friend WithEvents btnproses As Button
+    Friend WithEvents List1 As ListView
+    Friend WithEvents cbokelamin As ComboBox
+    Friend WithEvents cboprodi As ComboBox
+    Friend WithEvents btnedit As Button
+    Friend WithEvents btnhapus As Button
+    Friend WithEvents btnkeluar As Button
+
+End Class
---
+++ latihan array multidimensi/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim arr(3, 1) As String
+        arr(0, 0) = "NPM"
+        arr(0, 1) = "NAMA"
+        arr(1, 0) = "JENIS KELAMIN"
+        arr(1, 1) = "PRODI"
+        arr(2, 0) = "LAKI-LAKI"
+        arr(2, 1) = "PEREMPUAN"
+        arr(3, 0) = "SISTEM INFORMASI"
+        arr(3, 1) = "SISTEM INFORMATIKA"
+        List1.GridLines = True
+        List1.View = View.Details
+
+        For baris = 0 To 1
+            For kolom = 0 To 1
+                List1.Columns.Add(arr(baris, kolom), 120)
+            Next kolom
+        Next baris
+        For baris = 2 To 2
+            For kolom = 0 To 1
+                cbokelamin.Items.Add(arr(baris, kolom))
+            Next kolom
+        Next baris
+        For baris = 3 To 3
+            For kolom = 0 To 1
+                cboprodi.Items.Add(arr(baris, kolom))
+            Next kolom
+        Next baris
+
+    End Sub
+    Private Sub btnproses_Click(sender As Object, e As EventArgs) Handles btnproses.Click
+        Dim arr(3) As String
+        arr(0) = NPM.Text
+        arr(1) = txtnama.Text
+        arr(2) = cbokelamin.Text
+        arr(3) = cboprodi.Text
+
+        Dim listitem As ListViewItem
+        listitem = New ListViewItem
+        listitem = List1.Items.Add(arr(0))
+        listitem.SubItems.Add(arr(1))
+        listitem.SubItems.Add(arr(2))
+        listitem.SubItems.Add(arr(3))
+        NPM.Text = NPM.Text + 1
+        cbokelamin.Text = ""
+        cboprodi.Text = ""
+        txtnama.Focus()
+    End Sub
+
+    Private Sub btnedit_Click(sender As Object, e As EventArgs) Handles btnedit.Click
+        If List1.SelectedItems.Count > 0 Then
+            List1.SelectedItems(0).SubItems(0).Text = NPM.Text
+            List1.SelectedItems(0).SubItems(1).Text = txtnama.Text
+            List1.SelectedItems(0).SubItems(2).Text = cbokelamin.Text
+            List1.SelectedItems(0).SubItems(3).Text = cboprodi.Text
+        End If
+
+    End Sub
+
+    Private Sub btnhapus_Click(sender As Object, e As EventArgs) Handles btnhapus.Click
+        If List1.SelectedItems.Count > 0 Then
+            List1.Items.Remove(List1.SelectedItems(0))
+        End If
+    End Sub
+
+    Private Sub btnkeluar_Click(sender As Object, e As EventArgs) Handles btnkeluar.Click
+        End
+    End Sub
+End Class
